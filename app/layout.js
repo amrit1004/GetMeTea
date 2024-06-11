43cffba1dcdfdf99f32f5d7ec871d4c53a5bed99
Amrit <amritsinghal2211@gmail.com>
2024-06-11 22:46:52 +0530

    login functionality done

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
+import SessionWrapper from "@/components/SessionWrapper";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,11 +14,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <SessionWrapper>
         <Navbar/>
         <div className="min-h-[87vh] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
         {children}
           </div>
         <Footer/>
+        </SessionWrapper>
         </body>
     </html>
   );
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,7 @@
+"use client"
 import React from "react";
 import Link from "next/link";
+import { signIn } from "next-auth/react";
 const Navbar = () => {
   return (<nav className="flex items-center justify-between h-16 px-4 text-white bg-gray-900">
     <div className="flex items-center justify-center text-lg font-bold logo">
@@ -15,7 +17,7 @@ const Navbar = () => {
     </ul> */}
     <div>
       <Link href={"/login"}>
-    <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Login</button>
+    <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" >Login</button>
       </Link>
       
     </div>
