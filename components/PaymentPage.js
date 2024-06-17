c6246cdc1cf74ba99864d12944c73317ef109ff1
Amrit <amritsinghal2211@gmail.com>
2024-06-17 10:14:03 +0530

    "Added resetForm function and refractor paymenrpage

--- components/PaymentPage.js
+++ components/PaymentPage.js
@@ -13,6 +13,9 @@ const PaymentPage = ({username}) => {
      getData();
    }, [])
    
+   const resetForm = () => {
+    setPaymentform({ name: '', message: '', amount: '' })
+ }
    const handleChange = (e) =>{
       setPaymentform({...paymentform ,[e.target.name]: e.target.value})
      }
@@ -49,6 +52,7 @@ const PaymentPage = ({username}) => {
         }
         var rzp1 = new Razorpay(options);
          rzp1.open();
+         resetForm();
    
     }
   return (
@@ -91,7 +95,7 @@ const PaymentPage = ({username}) => {
           </div>
           <input onChange={handleChange} value={paymentform.message} type="text" name='message' className="w-full p-3 rounded-lg bg-slate-800" placeholder="Enter Message" />
           <input onChange={handleChange} value={paymentform.amount} type="text" name="amount" className="w-full p-3 rounded-lg bg-slate-800" placeholder="Enter Amount" />
-          <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Pay</button>
+          <button onClick={()=>pay(Number.parseInt(paymentform.amount)*100)} type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Pay</button>
         </div>
           <div className="flex gap-2 mt-5">
             <button className="p-3 rounded-lg bg-slate-800" onClick={()=>pay(1000)}>Pay ₹10</button>
